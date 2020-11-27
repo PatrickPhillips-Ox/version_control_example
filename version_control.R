15b59b00298b261a9457143b4b1fd892bb4dcdbb
Patrick <patrickphillips@Patricks-MacBook-Air.local>
2020-11-27 15:40:14 +0000

    Added curve fitting

--- version_control.R
+++ version_control.R
@@ -1,4 +1,5 @@
-# Version control with RStudio, Git and Github
+# Version control with RStudio, Git and Github (intro video)
+
 # changes made tracked by git = local environment
 # create a commit (saves differences, stores with description, can get back to prev versions)
 # commit each time before delete
@@ -18,8 +19,25 @@
 # 2. clone locally
 # 3. commit 
 
-
-
+# Github tutorial https://github.com/MalikaIhle/Introduction-RStudio-Git-GitHub
 
 mydata = read.csv("example_data.csv")
-plot(mydata$xdata,mydata$ydata)
+plot(mydata$xdata,mydata$ydata,col='red')
+
+# some guesses for the parameters.
+p1 = 1
+p2 = 0.2
+
+# do the fit
+fit = nls(ydata ~ p1*cos(p2*xdata) + p2*sin(p1*xdata), data = mydata, start = list(p1=p1,p2=p2))
+
+#Plot the fitted line
+new = data.frame(xdata = seq(min(mydata$xdata),max(mydata$xdata),len=200))
+lines(new$xdata,predict(fit,newdata=new))
+
+
+
+
+
+
+
